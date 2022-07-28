7f4eb28215764bfdb6d18ad009e9d9361ff9e559
sambit <sambitmahala653@gmail.com>
2022-07-28 20:23:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("")
+  const [list, setList] = useState([]);
+ 
+  
+  const addElem = (e) => {
+     e.preventDefault()
+    if(!text)
+    {
+
+    }
+    else
+    {
+      setList(() => [...list, text]);
+      setText('')
+    }
+    
+    //console.log(list)
+  };
+  const deleteTodo = (id) => {
+    const updatedItem=list.filter((td,index)=>{
+    return index !==id
+     } )
+     setList(updatedItem)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="hlist1">
+        <h2>ToDoList</h2>
       </header>
+      <form onClick={addElem}>
+        <label className="llabel">what do you want to do today?</label>
+        <br />
+        <input
+          value={text}
+           onChange={(event) => setText(event.target.value)}
+           className="input"
+          type="text"
+        />
+        <button className="button">Submit</button>
+      </form>
+      {list.map(
+        (td, index) =>{
+          return(
+            <div key={index}>
+              <br />
+              <div >
+                <input type="checkbox" />
+                <label>{td}</label>
+              <button  key={index}
+              onClick={()=>deleteTodo(index)}
+              
+              className="button">delete</button>
+            </div>
+            </div>
+          
+            )}
+            
+      )}
     </div>
   );
 }
